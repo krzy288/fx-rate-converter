b7e77456139d402a642a507de78ce58ea8a24518
Krzysztof <krzysztof.olejniczak288@gmail.com>
2025-07-22 11:16:58 +0200

    add foto of Rudy

--- backend/static/js/welcome.js
+++ backend/static/js/welcome.js
@@ -3,3 +3,9 @@ window.onload = function () {
     const now = new Date().toLocaleTimeString();
     messageBox.innerText = `Page loaded successfully at ${now}`;
 };
+
+
+function showImage() {
+  const container = document.getElementById('image-container');
+  container.style.display = 'block';
+}
